0ca1b4ef1665069a1e22e93bcb0bc8978c233b4b
Sarah L <alaskalaska18@yandex.ru>
2024-10-07 20:39:49 +0300

    started

---
+++ src/components/mainBlock/mainBlock.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const MainBlock = () => {
+
+  return (
+
+    
+    <div className="w-[1366px] h-[3642px] flex">
+    <div className="w-[1416px] bg-blue-500">
+      <h1 className="text-white">Первая секция</h1>
+    </div>
+  
+    <div className="w-[444px] bg-green-500">
+      <h1 className="text-white">Вторая секция</h1>
+    </div>
+  </div>
+  
+  )
+}
+
+export default MainBlock
