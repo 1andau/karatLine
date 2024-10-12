5a0867da68d4ade861d899538c3095bc6ab31b94
Sarah L <alaskalaska18@yandex.ru>
2024-10-12 18:01:06 +0300

    sort part

--- src/components/mainBlock/mainBlock.jsx
+++ src/components/mainBlock/mainBlock.jsx
@@ -55,18 +55,16 @@ const MainBlock = () => {
 
   return (
 
-
 <div className="flex justify-center items-center h-screen w-screen">
   <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mx-auto max-w-7xl h-full">
-    <div className="w-full md:w-2/3 bg-white-500 overflow-y-auto p-4">
-      <h1 className="text-[#2E2E30]">Первая секция</h1>
+    <div className="w-full md:w-2/2 bg-white-500 overflow-y-auto p-4">
       <div className="grid grid-cols-2 gap-2">
-        <img src="/assets/1.png" alt="Image 1" className="w-full h-auto" />
-        <img src="/assets/2.png" alt="Image 2" className="w-full h-auto" />
-        <img src="/assets/3.png" alt="Image 3" className="w-full h-auto" />
-        <img src="/assets/4.png" alt="Image 4" className="w-full h-auto" />
-        <img src="/assets/5.png" alt="Image 5" className="w-full h-auto" />
-        <img src="/assets/6.png" alt="Image 6" className="w-full h-auto" />
+        <img src="/assets/1.png" alt="Image 1" className="w-full h-auto max-h-62" />
+        <img src="/assets/2.png" alt="Image 2" className="w-full h-auto max-h-62" />
+        <img src="/assets/3.png" alt="Image 3" className="w-full h-auto max-h-62" />
+        <img src="/assets/4.png" alt="Image 4" className="w-full h-auto max-h-62" />
+        <img src="/assets/5.png" alt="Image 5" className="w-full h-auto max-h-62" />
+        <img src="/assets/6.png" alt="Image 6" className="w-full h-auto max-h-62" />
       </div>
       <div className="mt-4">
         <ul className="flex flex-wrap text-[#2E2E30] text-lg font-inter list-none p-0 space-x-4">
@@ -116,17 +114,17 @@ const MainBlock = () => {
 
       <div className="mt-4 p-4 bg-[#F4F5F6]">
         <h3 className="text-lg mb-2 text-[#2E2E30]">Характеристики</h3>
-        <table className="w-full border-separate border-spacing-x-4">
+        <table className="w-full border-separate border-spacing-x-2">
           <tbody>
             {left.map((leftItem, index) => (
               <tr key={index}>
-                <td className="border-b border-[#808080] py-2 px-4 text-[#2E2E30]">
-                  <div className="flex justify-between">
+                <td className="border-b border-[#808080] py-1 px-2 text-[#2E2E30]">
+                  <div className="flex justify-between ">
                     <span>{leftItem}</span>
                     <span>{right[index]}</span>
                   </div>
                 </td>
-                <td className="border-b border-[#808080] py-2 px-4 text-[#2E2E30]">
+                <td className="border-b border-[#808080] py-1 px-2 text-[#2E2E30]">
                   <div className="flex justify-between">
                     <span>{leftItem}</span>
                     <span>{right[index]}</span>
@@ -138,15 +136,70 @@ const MainBlock = () => {
         </table>
       </div>
     </div>
-    <div className="w-full md:w-1/3 bg-green-500 overflow-y-auto p-4">
-      <h1 className="text-[#2E2E30] uppercase">Вторая секция</h1>
-      <p className="text-[#2E2E30]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+
+  <div className="w-full md:w-1/2 bg-black-500 overflow-y-auto p-4">
+
+    <div className="flex items-center justify-between mt-4">
+      <img src="/assets/icons/1.svg" alt="Icon 1" className="w-6 h-6" />
+      <img src="/assets/icons/2.svg" alt="Icon 1" className="w-6 h-6" />
+      <img src="/assets/icons/3.svg" alt="Icon 1" className="w-6 h-6" />
+      <img src="/assets/icons/2.svg" alt="Icon 1" className="w-6 h-6" />
+      <span className="text-[#2E2E30] text-xs">Серьги из золота</span>
+
+      <img src="/assets/icons/2.svg" alt="Icon 1" className="w-5 h-5" />
+      <span className="text-[#2E2E30] text-xs">Золотые гвоздики с фианитами</span>
+
     </div>
-  </div>
+
+    <div className="flex items-center justify-between mt-4">
+      <div className="flex items-center">
+        <span className="text-[#2E2E30] text-xs mr-2 underline decoration-dotted">18 отзывов</span>
+        <img src="/assets/icons/star.svg" alt="Star" className="w-4 h-4 mr-1"/>
+        <img src="/assets/icons/star.svg" alt="Star" className="w-4 h-4 mr-1"/>
+        <img src="/assets/icons/star.svg" alt="Star" className="w-4 h-4 mr-1"/>
+        <img src="/assets/icons/star.svg" alt="Star" className="w-4 h-4 mr-1"/>
+        <img src="/assets/icons/star.svg" alt="Star" className="w-4 h-4 mr-1"/>
+        <span className="text-[#2E2E30] text-xs ml-2">5.0</span>
+      </div>
+      <span className="text-[#2E2E30] text-xs">Код: 000-383753</span>
+    </div>
+
+    <h2 className="text-[#2E2E30]  font-bold mt-4 text-3xl	">Золотые серьги гвоздики с фианитами</h2>
+    <div className="bg-[#F4F5F6] p-4 mt-4">
+
+    <div className="border border-[#CAA212] p-2 flex items-center justify-between w-11/12 md:w-8/12">
+        <span className="text-[#2E2E30] text-sm">16 501 ₽</span>
+        <div className="flex items-center">
+          <span className="text-[#2E2E30] text-sm mr-2">По коду ЛЕТО</span>
+          <img src="/assets/icons/arrow.svg" alt="Arrow" className="w-4 h-4" />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between pt-2.5">
+        <div className="flex items-center">
+        <span class="text-[#2E2E30] text-xl text-sm mr-2">20 902 ₽</span>
+          <span class="text-[#2E2E30] text-xl line-through mr-2">22 680 ₽</span>
+          <div class="bg-[#D43939] text-white text-sm p-2 rounded">15%</div>
+          </div>
+          <img src="/assets/icons/down.svg" alt="Arrow" class="w-4 h-4" />
+
+          </div> 
+
+    </div>
+
+    <div className="bg-[#F4F5F6] p-4 mt-4">
+    <div className="flex items-center">
+    <h1 className='text-[#2E2E30] text-xl ine-through mr-2'>Размер</h1>
+<span className='text-[#2E2E30] underline decoration-dotted ine-through mr-2 pt-1	'>Как выбрать размер</span>
+
+</div>
 </div>
 
+  </div>
+    
+
+  </div>
+</div>
 
   )
 }
